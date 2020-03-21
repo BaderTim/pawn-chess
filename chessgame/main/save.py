98ec4fb1b4bc6ce08415dc82775d8bede954cc39
BaderTim <bader.tim_alex-it19@it.dhbw-ravensburg.de>
2020-03-21 11:10:36 +0100

    made error messages caused by loading corrupted files more exact

--- chessgame/main/save.py
+++ chessgame/main/save.py
@@ -45,17 +45,17 @@ class Save:
             for counter, line in enumerate(file_lines):
                 if counter == 0:
                     if ("m" or "ki") not in line:
-                        print(f"\nFehler: Datei '{self.save_file}' scheint einen fehlerhaften Spielmodi zu haben.\n")
+                        print(f"\nFehler: Datei '{self.save_file}' scheint einen fehlerhaften Spielmodi in Zeile {counter+1} zu haben.\n")
                         return None
                     save_load.append(line.replace("\n", ""))
                 else:
                     data = line.replace(" ", "").replace("\n", "").split("_")
                     if data[0] != "w" and data[0] != "b":
-                        print(f"\nFehler: Datei '{self.save_file}' scheint fehlerhafte Farben zu haben.\n")
+                        print(f"\nFehler: Datei '{self.save_file}' scheint fehlerhafte Farben in Zeile {counter+1} zu haben.\n")
                         return None
                     cords = data[1].split("#")
                     if int(cords[0]) < 1 or int(cords[0]) > 8 or int(cords[1]) < 1 or int(cords[1]) > 8:
-                        print(f"\nFehler: Datei '{self.save_file}' scheint fehlerhafte Koordinaten zu haben.\n")
+                        print(f"\nFehler: Datei '{self.save_file}' scheint fehlerhafte Koordinaten in Zeile {counter+1} zu haben.\n")
                         return None
                     figures.append(Pawn(pos_x=int(cords[0]), pos_y=int(cords[1]), color=data[0]))
             save_load.append(figures)
@@ -63,7 +63,7 @@ class Save:
         except FileNotFoundError:
             print(f"\nFehler: konnte '{self.save_file}' nicht finden.\n")
         except TypeError:
-            print(f"\nFehler: Datei '{self.save_file}' scheint fehlerhaft zu sein.")
+            print(f"\nFehler: Datei '{self.save_file}' scheint fehlerhafte Koordinaten zu haben.")
         return None
 
     def save_game(self):
