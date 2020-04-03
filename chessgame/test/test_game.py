6811afcd020fd9d20d191cce17df0865f549395a
xenaletters <62646699+xenaletters@users.noreply.github.com>
2020-04-03 16:42:47 +0200

    Create test_game.py
    Konnte nich testen ob es funktioniert

---
+++ chessgame/test/test_game.py
@@ -0,0 +1,27 @@
+import unittest
+from chessgame.main.game import Game
+from chessgame.main.consts import MODE_MULTI
+from chessgame.main.consts import COLOR_BLACK
+from chessgame.main.consts import COLOR_WHITE
+from chessgame.main.pawn import Pawn
+
+class GameTest(unittest.TestCase):
+    '''
+        Testclass for Game
+    '''
+    def test_start_game(self):
+        '''
+        Test for game.start_game
+            - tests if the array is produced correctly
+        '''
+        game = Game(MODE_MULTI)
+        pawns = []
+        for counter in range(8):
+            pawns.append(Pawn(counter + 1, 2, COLOR_WHITE))
+            pawns.append(Pawn(counter + 1, 7, COLOR_BLACK))
+        game.start_game(game.game_mode)
+        self.assertEqual(len(pawns), len(game.figures))
+        self.assertEqual(len(game.figures), 16)
+        for counter in range(16):
+            self.assertEqual(game.figures[counter].get_position, pawns[counter].get_position)
+            
