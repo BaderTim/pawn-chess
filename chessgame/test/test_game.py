134400755c63d98c628ab18565d57b2447d0ee07
BaderTim <bader.tim_alex-it19@it.dhbw-ravensburg.de>
2020-04-04 16:14:50 +0200

    added tests for get_figure and check_for_hit from game.py

--- chessgame/test/test_game.py
+++ chessgame/test/test_game.py
@@ -1,43 +1,87 @@
 # pylint: disable=C
 import unittest
 from chessgame.main.game import Game
-from chessgame.main.consts import MODE_MULTI
 from chessgame.main.consts import COLOR_BLACK
 from chessgame.main.consts import COLOR_WHITE
-from chessgame.main.consts import ACT_SAVE
-from chessgame.main.consts import ACT_STOP
+from chessgame.main.consts import PLAYER_BLACK
+from chessgame.main.consts import PLAYER_WHITE
 from chessgame.main.pawn import Pawn
+
 
 class GameTest(unittest.TestCase):
     '''
         Testclass for Game
     '''
-    def test_start_game(self):
-        '''
-        Test for game.start_game
-            - tests if the array is produced correctly
-        '''
-        game = Game(MODE_MULTI)
-        pawns = []
-        for counter in range(8):
-            pawns.append(Pawn(counter + 1, 2, COLOR_WHITE))
-            pawns.append(Pawn(counter + 1, 7, COLOR_BLACK))
-        game.start_game(game.game_mode)
-        self.assertEqual(len(pawns), len(game.figures))
-        self.assertEqual(len(game.figures), 16)
-        for counter in range(16):
-            self.assertEqual(game.figures[counter].get_position, pawns[counter].get_position)
 
-    def test_save_game(self):
-        '''
-        Test for game.same_game
-        '''
-        game = Game(MODE_MULTI)
-        game.save_game(ACT_SAVE)
-        self.assertTrue(game.saved)
-        game.saved = True
-        game.save_game(ACT_STOP)
-        self.assertTrue(game.end_game)
-        game.saved = False
-        game.save_game(ACT_STOP)
-        
+    def test_get_figure_machine_input(self):
+        game = Game("test")
+        game.figures = []
+        for counter in range(8):
+            game.figures.append(Pawn(counter + 1, 2, COLOR_WHITE))
+            machine_input = f"{counter + 1}::2"
+            self.assertEqual(game.get_figure(machine_input).get_pos_x(), Pawn(counter + 1, 2, COLOR_WHITE).get_pos_x())
+            self.assertEqual(game.get_figure(machine_input).get_pos_y(), Pawn(counter + 1, 2, COLOR_WHITE).get_pos_y())
+            self.assertEqual(game.get_figure(machine_input).get_color(), Pawn(counter + 1, 2, COLOR_WHITE).get_color())
+            game.figures.append(Pawn(counter + 1, 7, COLOR_BLACK))
+            machine_input = f"{counter + 1}::7"
+            self.assertEqual(game.get_figure(machine_input).get_pos_x(), Pawn(counter + 1, 7, COLOR_BLACK).get_pos_x())
+            self.assertEqual(game.get_figure(machine_input).get_pos_y(), Pawn(counter + 1, 7, COLOR_BLACK).get_pos_y())
+            self.assertEqual(game.get_figure(machine_input).get_color(), Pawn(counter + 1, 2, COLOR_BLACK).get_color())
+
+    def test_get_figure_user_input(self):
+        game = Game("test")
+        game.figures = []
+        for counter in range(8):
+            game.figures.append(Pawn(counter + 1, 2, COLOR_WHITE))
+            user_input = f"{str(chr(counter+65))}2"
+            self.assertEqual(game.get_figure(user_input).get_pos_x(), Pawn(counter + 1, 2, COLOR_WHITE).get_pos_x())
+            self.assertEqual(game.get_figure(user_input).get_pos_y(), Pawn(counter + 1, 2, COLOR_WHITE).get_pos_y())
+            self.assertEqual(game.get_figure(user_input).get_color(), Pawn(counter + 1, 2, COLOR_WHITE).get_color())
+            game.figures.append(Pawn(counter + 1, 7, COLOR_BLACK))
+            user_input = f"{str(chr(counter+65))}7"
+            self.assertEqual(game.get_figure(user_input).get_pos_x(), Pawn(counter + 1, 7, COLOR_BLACK).get_pos_x())
+            self.assertEqual(game.get_figure(user_input).get_pos_y(), Pawn(counter + 1, 7, COLOR_BLACK).get_pos_y())
+            self.assertEqual(game.get_figure(user_input).get_color(), Pawn(counter + 1, 2, COLOR_BLACK).get_color())
+
+    def test_check_for_hit_none(self):
+        game = Game("test")
+        game.figures = []
+        for counter in range(8):
+            game.figures.append(Pawn(counter + 1, 2, COLOR_WHITE))
+            game.figures.append(Pawn(counter + 1, 7, COLOR_BLACK))
+        fig_counter_pre = len(game.figures)
+        game.check_for_hit(5, 5, PLAYER_BLACK)
+        fig_counter_pos = len(game.figures)
+        self.assertEqual(fig_counter_pre, fig_counter_pos)
+
+    def test_check_for_hit_true(self):
+        game = Game("test")
+        game.figures = []
+        for counter in range(8):
+            game.figures.append(Pawn(counter + 1, 2, COLOR_WHITE))
+            game.figures.append(Pawn(counter + 1, 7, COLOR_BLACK))
+        fig_counter_pre = len(game.figures)
+        game.check_for_hit(1, 2, PLAYER_BLACK)
+        fig_counter_pos = len(game.figures)
+        self.assertNotEqual(fig_counter_pre, fig_counter_pos)
+
+        fig_counter_pre = fig_counter_pos
+        game.check_for_hit(1, 7, PLAYER_WHITE)
+        fig_counter_pos = len(game.figures)
+        self.assertNotEqual(fig_counter_pre, fig_counter_pos)
+
+    def test_check_for_hit_false(self):
+        game = Game("test")
+        game.figures = []
+        for counter in range(8):
+            game.figures.append(Pawn(counter + 1, 2, COLOR_WHITE))
+            game.figures.append(Pawn(counter + 1, 7, COLOR_BLACK))
+        fig_counter_pre = len(game.figures)
+        game.check_for_hit(1, 2, PLAYER_WHITE)
+        fig_counter_pos = len(game.figures)
+        self.assertEqual(fig_counter_pre, fig_counter_pos)
+
+        fig_counter_pre = fig_counter_pos
+        game.check_for_hit(1, 7, PLAYER_BLACK)
+        fig_counter_pos = len(game.figures)
+        self.assertEqual(fig_counter_pre, fig_counter_pos)
