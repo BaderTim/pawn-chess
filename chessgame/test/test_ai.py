6cb6720230f96bcf8de6e1f32212164c73ba07b6
Manuel Emmert <manuel.emmert@web.de>
2020-04-05 14:11:03 +0200

    Finalized ai method tests

--- chessgame/test/test_ai.py
+++ chessgame/test/test_ai.py
@@ -23,3 +23,16 @@ class AITest(unittest.TestCase):
         game.figures = [Pawn(4,6,consts.COLOR_BLACK),Pawn(3,5,consts.COLOR_WHITE),Pawn(4,5,consts.COLOR_WHITE),Pawn(5,5,consts.COLOR_WHITE)]
         confdict = {'4::6':{consts.MV_FWD2 : 0, consts.MV_FWD1 : 0, consts.MV_LEFT : (100/5)+20, consts.MV_RIGHT : (100/5)+20}}
         self.assertEqual(game.ai_moves(),confdict)
+    
+    def test_get_best_move(self):
+        game = Game("test")
+        testdict = ("2::6", {consts.MV_FWD2 : 0, consts.MV_FWD1 : (100/5), consts.MV_LEFT: (100/5) + 20, consts.MV_RIGHT: 0})
+        retval = ["2::6" , consts.MV_LEFT, (100/5) + 20]
+        self.assertEqual(game.get_best_move(testdict), retval)
+    
+    def test_ai_decide(self):
+        game = Game("test")
+        testlist = [['2::7', 'm2', (100/5)], ['4::5', 'r', (100/4) +20], ['6::2', 'm', 100]]
+        self.assertEqual(game.ai_decide(testlist),['6::2','m',100])
+        testlist = [['2::7', 'm2', (100/5)], ['4::5', 'r', (100/4) +20], ['6::2', 'm', 100], ['5::2', 'm', 100]]
+        self.assertIn(game.ai_decide(testlist), (['6::2', 'm', 100], ['5::2', 'm', 100]))
