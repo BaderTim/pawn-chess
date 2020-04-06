08ec45910aca59cc5101a54b7b722afd27fca6f0
Manuel Emmert <manuel.emmert@web.de>
2020-04-06 09:47:15 +0200

    Final? Tests

---
+++ chessgame/test/test_main.py
@@ -0,0 +1,35 @@
+#pylint: disable=C
+
+import unittest
+import main
+import sys
+import chessgame.test.test_game_std as std
+
+class MainTest(unittest.TestCase):
+
+    def test_nav_back(self):
+        std.stub_stdin(self,"l\nb\nx\nx")
+        std.stub_stdouts(self)
+        self.assertEqual(main.main(), 2)
+    
+    def test_nav_quit_main(self):
+        std.stub_stdin(self,"x")
+        std.stub_stdouts(self)
+        self.assertEqual(main.main(), 3)
+
+    def test_nav_quit_new(self):
+        std.stub_stdin(self,"n\nx\nx")
+        std.stub_stdouts(self)
+        self.assertEqual(main.main(), 1)
+    
+    def test_nav_new(self):
+        std.stub_stdin(self,"n\nm\nx\nx")
+        std.stub_stdouts(self)
+        self.assertEqual(main.main(), 0)
+    
+    def test_mode_invalid(self):
+        std.stub_stdin(self,"n\nfoo_bar\nx")
+        std.stub_stdouts(self)
+        main.main()
+        output = sys.stdout.getvalue()
+        self.assertIn("Fehler: 'foo_bar' konnte nicht zugeordnet werden. Bitte versuche es erneut.", output)
--- main.py
+++ main.py
@@ -12,6 +12,13 @@ def main():
 
     User can select between different game modes
     or load a saved game
+
+    Returns:
+        test_result {int}:
+            0 new game has been started
+            1 Game has been stopped in mode selection
+            2 Game went back to the main menu from load menu
+            3 Game has been stopped from main menu
     """
     while True:
         print("\n\n\n---WELCOME TO BAUERNSCHACH---\n")
@@ -22,15 +29,16 @@ def main():
             game_mode = input("Eingabe: ").lower()
             if game_mode in (consts.MODE_AI, consts.MODE_MULTI):
                 Game(game_mode)
-            elif game_mode == consts.ACT_STOP:
-                break
-            elif game_mode != consts.ACT_BACK:
+                return 0
+            if game_mode == consts.ACT_STOP:
+                return 1
+            if game_mode != consts.ACT_BACK:
                 print(f"Fehler: '{game_mode}' konnte nicht zugeordnet werden. Bitte versuche es erneut.")
         elif user_input == consts.ACT_LOAD:
             Game(consts.ACT_LOAD)
+            return 2
         elif user_input == consts.ACT_STOP:
-            break
-
+            return 3
 
 if __name__ == '__main__':
     main()
